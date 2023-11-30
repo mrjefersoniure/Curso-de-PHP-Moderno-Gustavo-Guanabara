a6f217f0ad3b86772d4f67a45f44a1e861867255
mrjefersoniure <jefersoniurematos@gmail.com>
2023-11-29 21:06:13 -0300

    Atualizacao_da_questao 3.2 API

--- ex004Desafios3.2/cad.php
+++ ex004Desafios3.2/cad.php
@@ -13,13 +13,13 @@
     <main>
         
         <?php 
-            $inicio = ("m-d-Y". strtotime("-7 days"));
-            $fim = ("m-d-Y");
+            $inicio = date("m-d-Y", strtotime("-7 days"));
+            $fim = date("m-d-Y");
             //link da api dolar do banco central
-            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$skip=0&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+            $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.($inicio).'\'&@dataFinalCotacao=\''.($fim).'\'&$top=1&$skip=0&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
             $dados = json_decode(file_get_contents($url), true);
-
+            
             $cotacao = $dados["value"][0]["cotacaoCompra"];
 
            // $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY );
